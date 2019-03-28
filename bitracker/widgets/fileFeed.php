9590ab7f39167e7ceed88f63d19b2f8a16f791db
GaalexxC <gary@devcu.com>
2019-03-28 19:49:45 -0400

    IPS 4.3 >> 4.4

---
+++ bitracker/widgets/fileFeed.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker fileFeed Widget
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     28 MAR 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\widgets;
+
+ /* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Torrents Entry fileFeed Widget
+ */
+class _fileFeed extends \IPS\Content\Widget
+{
+	/**
+	 * @brief	Widget Key
+	 */
+	public $key = 'fileFeed';
+	
+	/**
+	 * @brief	App
+	 */
+	public $app = 'bitracker';
+		
+	/**
+	 * @brief	Plugin
+	 */
+	public $plugin = '';
+	
+	/**
+	 * Class
+	 */
+	protected static $class = 'IPS\bitracker\File';
+
+	/**
+	* Init the widget
+	*
+	* @return	void
+	*/
+	public function init()
+	{
+		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'widgets.css', 'bitracker', 'front' ) );
+		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'bitracker.css', 'bitracker', 'front' ) );
+		parent::init();
+	}
+
+	/**
+	 * Specify widget configuration
+	 *
+	 * @param	null|\IPS\Helpers\Form	$form	Form object
+	 * @return	null|\IPS\Helpers\Form
+	 */
+	public function configuration( &$form=null )
+	{
+		$form = parent::configuration( $form );
+
+		if ( \IPS\Application::appIsEnabled( 'nexus' ) and \IPS\Settings::i()->bit_nexus_on )
+		{
+			$options = array(
+				'free'		=> 'file_free',
+				'paid'		=> 'file_paid',
+				'any'		=> 'any'
+			);
+
+			$form->add( new \IPS\Helpers\Form\Radio( 'file_cost_type', isset( $this->configuration['file_cost_type'] ) ? $this->configuration['file_cost_type'] : 'any', TRUE, array( 'options'	=> $options ) ) );
+		}
+
+		return $form;
+	}
+
+	/**
+	 * Get where clause
+	 *
+	 * @return	array
+	 */
+	protected function buildWhere()
+	{
+		$where = parent::buildWhere();
+
+		if ( \IPS\Application::appIsEnabled( 'nexus' ) and \IPS\Settings::i()->bit_nexus_on )
+		{
+			if( isset( $this->configuration['file_cost_type'] ) )
+			{
+				switch( $this->configuration['file_cost_type'] )
+				{
+					case 'free':
+						$where[] = array( "( ( file_cost='' OR file_cost IS NULL ) AND ( file_nexus='' OR file_nexus IS NULL ) )" );
+						break;
+					case 'paid':
+						$where[] = array( "( file_cost<>'' OR file_nexus>0 )" );
+						break;
+				}
+			}
+		}
+
+		return $where;
+	}
+}
