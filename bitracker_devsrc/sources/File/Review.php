68f3b5480abc8f9859f68b3d71f3106bfe5414dd
GaalexxC <gary@devcu.com>
2019-03-28 23:29:02 -0400

    IPS 4.3 >> 4.4

---
+++ bitracker_devsrc/sources/File/Review.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker File Review Model
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     28 MAR 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\File;
+
+ /* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * File Review Model
+ */
+class _Review extends \IPS\Content\Review implements \IPS\Content\EditHistory, \IPS\Content\Hideable, \IPS\Content\Searchable, \IPS\Content\Embeddable
+{
+	use \IPS\Content\Reactable, \IPS\Content\Reportable;
+	
+	/**
+	 * @brief	[ActiveRecord] Multiton Store
+	 */
+	protected static $multitons;
+	
+	/**
+	 * @brief	[Content\Comment]	Item Class
+	 */
+	public static $itemClass = 'IPS\bitracker\File';
+	
+	/**
+	 * @brief	[ActiveRecord] Database Table
+	 */
+	public static $databaseTable = 'bitracker_reviews';
+	
+	/**
+	 * @brief	[ActiveRecord] Database Prefix
+	 */
+	public static $databasePrefix = 'review_';
+	
+	/**
+	 * @brief	Database Column Map
+	 */
+	public static $databaseColumnMap = array(
+		'item'				=> 'fid',
+		'author'			=> 'mid',
+		'author_name'		=> 'author_name',
+		'content'			=> 'text',
+		'date'				=> 'date',
+		'ip_address'		=> 'ip',
+		'edit_time'			=> 'edit_time',
+		'edit_member_name'	=> 'edit_name',
+		'edit_show'			=> 'append_edit',
+		'rating'			=> 'rating',
+		'votes_total'		=> 'votes',
+		'votes_helpful'		=> 'votes_helpful',
+		'votes_data'		=> 'votes_data',
+		'approved'			=> 'approved',
+		'author_response'	=> 'author_response',
+	);
+	
+	/**
+	 * @brief	Application
+	 */
+	public static $application = 'bitracker';
+	
+	/**
+	 * @brief	Title
+	 */
+	public static $title = 'bitracker_file_review';
+	
+	/**
+	 * @brief	Icon
+	 */
+	public static $icon = 'download';
+	
+	/**
+	 * @brief	[Content]	Key for hide reasons
+	 */
+	public static $hideLogKey = 'bitracker-files-rev';
+	
+	/**
+	 * Get URL for doing stuff
+	 *
+	 * @param	string|NULL		$action		Action
+	 * @return	\IPS\Http\Url
+	 */
+	public function url( $action=NULL )
+	{
+		return parent::url( $action )->setQueryString( 'tab', 'reviews' );
+	}
+
+	
+	/**
+	 * Get snippet HTML for search result display
+	 *
+	 * @param	array		$indexData		Data from the search index
+	 * @param	array		$authorData		Basic data about the author. Only includes columns returned by \IPS\Member::columnsForPhoto()
+	 * @param	array		$itemData		Basic data about the item. Only includes columns returned by item::basicDataColumns()
+	 * @param	array|NULL	$containerData	Basic data about the container. Only includes columns returned by container::basicDataColumns()
+	 * @param	array		$reputationData	Array of people who have given reputation and the reputation they gave
+	 * @param	int|NULL	$reviewRating	If this is a review, the rating
+	 * @param	string		$view			'expanded' or 'condensed'
+	 * @return	callable
+	 */
+	public static function searchResultSnippet( array $indexData, array $authorData, array $itemData, array $containerData = NULL, array $reputationData, $reviewRating, $view )
+	{
+		return \IPS\bitracker\File\Comment::searchResultSnippet( $indexData, $authorData, $itemData, $containerData, $reputationData, $reviewRating, $view );
+	}
+	
+	/**
+	 * Reaction type
+	 *
+	 * @return	string
+	 */
+	public static function reactionType()
+	{
+		return 'review_id';
+	}
+
+	/**
+	 * Get content for embed
+	 *
+	 * @param	array	$params	Additional parameters to add to URL
+	 * @return	string
+	 */
+	public function embedContent( $params )
+	{
+		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'embed.css', 'bitracker', 'front' ) );
+		return \IPS\Theme::i()->getTemplate( 'global', 'bitracker' )->embedFileReview( $this, $this->item(), $this->url()->setQueryString( $params ) );
+	}
+}
