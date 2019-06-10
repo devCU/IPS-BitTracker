844f12d68528c806bce899c5bc2ea4c46c520a7f
GaalexxC <gary@devcu.com>
2019-06-10 01:47:27 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/IpAddresses/DownloadLog.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker IP Address Lookup
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+ 
+namespace IPS\bitracker\extensions\core\IpAddresses;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * IP Address Lookup extension
+ */
+class _DownloadLog
+{
+	/**
+	 * Supported in the ACP IP address lookup tool?
+	 *
+	 * @return	bool
+	 * @note	If the method does not exist in an extension, the result is presumed to be TRUE
+	 */
+	public function supportedInAcp()
+	{
+		return TRUE;
+	}
+
+	/**
+	 * Supported in the ModCP IP address lookup tool?
+	 *
+	 * @return	bool
+	 * @note	If the method does not exist in an extension, the result is presumed to be TRUE
+	 */
+	public function supportedInModCp()
+	{
+		return TRUE;
+	}
+
+	/** 
+	 * Find Records by IP
+	 *
+	 * @param	string			$ip			The IP Address
+	 * @param	\IPS\Http\Url	$baseUrl	URL table will be displayed on or NULL to return a count
+	 * @return	\IPS\Helpers\Table|int|null
+	 */
+	public function findByIp( $ip, \IPS\Http\Url $baseUrl = NULL )
+	{
+		/* Return count */
+		if ( $baseUrl === NULL )
+		{
+			return \IPS\Db::i()->select( 'COUNT(*)', 'bitracker_downloads', array( "dip LIKE ?", $ip ) )->first();
+		}
+		
+		/* Init Table */
+		$table = new \IPS\Helpers\Table\Db( 'bitracker_downloads', $baseUrl, array( "dip LIKE ?", $ip ) );
+
+		$table->tableTemplate  = array( \IPS\Theme::i()->getTemplate( 'tables', 'core', 'admin' ), 'table' );
+		$table->rowsTemplate  = array( \IPS\Theme::i()->getTemplate( 'tables', 'core', 'admin' ), 'rows' );
+		
+		$table->include = array( 'dfid', 'dtime', 'dsize', 'dua', 'dmid', 'dip' );
+		$table->sortBy = $table->sortBy ?: 'dtime';
+		
+		/* Parsers */
+		$table->parsers = array(
+			'dfid'	=> function( $val )
+			{
+				try
+				{
+					$file = \IPS\bitracker\File::load( $val );
+					return \IPS\Theme::i()->getTemplate( 'global', 'core', 'global' )->basicUrl( $file->url(), TRUE, $file->name );
+				}
+				catch ( \OutOfRangeException $e )
+				{
+					return \IPS\Member::loggedIn()->language()->addToStack('content_deleted');
+				}
+			},
+			'dtime'	=> function( $val )
+			{
+				return (string) \IPS\DateTime::ts( $val );
+			},
+			'dsize'	=> function( $val )
+			{
+				return \IPS\Output\Plugin\Filesize::humanReadableFilesize( $val );
+			},
+			'dua'	=> function( $val )
+			{
+				return (string) \IPS\Http\Useragent::parse( $val );
+			},
+			'dmid'	=> function( $val )
+			{
+				$member = \IPS\Member::load( $val );
+				return \IPS\Theme::i()->getTemplate( 'global', 'core' )->userPhoto( $member, 'tiny' ) . ' ' . $member->link();
+			},
+		);
+		
+		/* Return */
+		return (string) $table;
+	}
+	
+	/**
+	 * Find IPs by Member
+	 *
+	 * @code
+	 	return array(
+	 		'::1' => array(
+	 			'ip'		=> '::1'// string (IP Address)
+		 		'count'		=> ...	// int (number of times this member has used this IP)
+		 		'first'		=> ... 	// int (timestamp of first use)
+		 		'last'		=> ... 	// int (timestamp of most recent use)
+		 	),
+		 	...
+	 	);
+	 * @endcode
+	 * @param	\IPS\Member	$member	The member
+	 * @return	array
+	 */
+	public function findByMember( $member )
+	{
+		return \IPS\Db::i()->select( "dip AS ip, COUNT(*) AS count, MIN(dtime) AS first, MAX(dtime) AS last", 'bitracker_downloads', array( "dmid=?", $member->member_id ), NULL, NULL, 'dip' )->setKeyField( 'ip' );
+	}	
+}
