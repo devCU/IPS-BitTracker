052bbfba787c84280fd0a75fe6d2c876a1716a86
GaalexxC <gary@devcu.com>
2019-06-10 09:12:03 -0400

    2.0.0 RC 1

--- bitracker/sources/Form/RatioRules.php
+++ bitracker/sources/Form/RatioRules.php
@@ -9,11 +9,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4x
  * @subpackage	BitTracker
- * @version     2.0.0 Beta 1
+ * @version     2.0.0 RC 1
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     28 MAR 2019
+ * @Updated     10 JUN 2019
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -84,7 +84,7 @@ class _RatioRules extends \IPS\Helpers\Form\KeyValue
 
 		$options = $this->addRatioRule( $options );
 
-		call_user_func_array( 'parent::__construct', func_get_args() );
+		\call_user_func_array( 'parent::__construct', \func_get_args() );
 		$this->keyField = new \IPS\Helpers\Form\Number( "{$name}[key]", isset( $defaultValue['key'] ) ? $defaultValue['key'] : NULL, FALSE, isset( $options['key'] ) ? $options['key'] : array() );
 		$this->valueField = new \IPS\Helpers\Form\Select( "{$name}[value]", isset( $defaultValue['value'] ) ? $defaultValue['value'] : NULL, FALSE, isset( $options['value'] ) ? $options['value'] : array() );
 	}
