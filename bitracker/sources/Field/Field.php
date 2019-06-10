072fcc193d84ddc7a3cf45bb8302932f33129902
GaalexxC <gary@devcu.com>
2019-06-10 05:27:17 -0400

    2.0.0 RC 1

--- bitracker/sources/Field/Field.php
+++ bitracker/sources/Field/Field.php
@@ -9,11 +9,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4x
  * @subpackage	BitTracker
- * @version     2.0.0 Beta 1
+ * @version     2.0.0 RC 1
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     28 MAR 2019
+ * @Updated     10 JUN 2019
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -96,7 +96,7 @@ class _Field extends \IPS\CustomField
 	 */
 	protected static $restrictions = array(
 		'app'		=> 'bitracker',
-		'module'	=> 'bitracker',
+		'module'	=> 'configure',
 		'prefix'	=> 'fields_',
 	);
 
