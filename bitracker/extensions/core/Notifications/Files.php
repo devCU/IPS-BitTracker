e6a5c7ddaae720aa32d02bf0fbdaab61bb5adaf1
GaalexxC <gary@devcu.com>
2019-06-10 01:47:54 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/Notifications/Files.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Notification Options
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\Notifications;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Notification Options
+ */
+class _Files
+{
+	/**
+	 * Get configuration
+	 *
+	 * @param	\IPS\Member	$member	The member
+	 * @return	array
+	 */
+	public function getConfiguration( $member )
+	{		
+		return array(
+			'new_torrent_version'	=> array( 'default' => array( 'email' ), 'disabled' => array() ),
+		);
+	}
+	
+	/**
+	 * Parse notification: new_content
+	 *
+	 * @param	\IPS\Notification\Inline	$notification	The notification
+	 * @return	array
+	 * @code
+	 	return array(
+	 		'title'		=> "Mark has replied to A Topic",			// The notification title
+	 		'url'		=> \IPS\Http\Url\Friendly::internal( ... ),	// The URL the notification should link to
+	 		'content'	=> "Lorem ipsum dolar sit",					// [Optional] Any appropriate content. Do not format this like an email where the text
+	 																// explains what the notification is about - just include any appropriate content.
+	 																// For example, if the notification is about a post, set this as the body of the post.
+	 		'author'	=>  \IPS\Member::load( 1 ),					// [Optional] The user whose photo should be displayed for this notification
+	 	);
+	 * @endcode
+	 */
+	public function parse_new_torrent_version( $notification )
+	{
+		$item = $notification->item;
+		if ( !$item )
+		{
+			throw new \OutOfRangeException;
+		}
+
+		return array(
+			'title'		=> \IPS\Member::loggedIn()->language()->addToStack( 'notification__new_torrent_version', FALSE, array( 'sprintf' => array( $item->author()->name, $item->mapped('title') ) ) ),
+			'url'		=> $notification->item->url(),
+			'content'	=> $notification->item->content(),
+			'author'	=> $notification->extra ?: $notification->item->author(),
+			'unread'	=> (bool) ( $item->unread() )
+		);
+	}
+}
