3184130192f09819a7f1c0a4940db94248037246
GaalexxC <gary@devcu.com>
2019-06-10 01:48:04 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/Queue/Follow.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Follow
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\Queue;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Background Task
+ */
+class _Follow
+{
+	/**
+	 * Run Background Task
+	 *
+	 * @param	mixed						$data	Data as it was passed to \IPS\Task::queue()
+	 * @param	int							$offset	Offset
+	 * @return	int							New offset
+	 * @throws	\IPS\Task\Queue\OutOfRangeException	Indicates offset doesn't exist and thus task is complete
+	 */
+	public function run( $data, $offset )
+	{
+		$category	= \IPS\bitracker\Category::load( $data['category_id'] );
+		$member		= \IPS\Member::load( $data['member_id'] );
+		$newOffset	= \IPS\bitracker\File::_sendNotificationsBatch( $category, $member, $offset );
+
+		if( $newOffset === NULL )
+		{
+			throw new \IPS\Task\Queue\OutOfRangeException;
+		}
+
+		return $newOffset;
+	}
+	
+	/**
+	 * Get Progress
+	 *
+	 * @param	mixed					$data	Data as it was passed to \IPS\Task::queue()
+	 * @param	int						$offset	Offset
+	 * @return	array( 'text' => 'Doing something...', 'complete' => 50 )	Text explaining task and percentage complete
+	 * @throws	\OutOfRangeException	Indicates offset doesn't exist and thus task is complete
+	 */
+	public function getProgress( $data, $offset )
+	{
+		$category			= \IPS\bitracker\Category::load( $data['category_id'] );
+		$complete			= $data['followerCount'] ? round( 100 / $data['followerCount'] * $offset, 2 ) : 100;
+
+		return array( 'text' => \IPS\Member::loggedIn()->language()->addToStack('backgroundQueue_follow', FALSE, array( 'htmlsprintf' => array( \IPS\Theme::i()->getTemplate( 'global', 'core', 'global' )->basicUrl( $category->url(), TRUE, $category->_title, FALSE ) ) ) ), 'complete' => $complete );
+	}		
+}
