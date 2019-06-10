42ca93543b8d50d48dc0c70245877e6974eecbe9
GaalexxC <gary@devcu.com>
2019-06-10 01:47:49 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/ModeratorPermissions/Files.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Moderator Permissions
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\ModeratorPermissions;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Moderator Permissions
+ */
+class _Files
+{
+	/**
+	 * Get Permissions
+	 *
+	 * @code
+	 	return array(
+	 		'key'	=> 'YesNo',	// Can just return a string with type
+	 		'key'	=> array(	// Or an array for more options
+	 			'YesNo',			// Type
+	 			array( ... ),		// Options (as defined by type's class)
+	 			'prefix',			// Prefix
+	 			'suffix',			// Suffix
+	 		),
+	 		...
+	 	);
+	 * @endcode
+	 * @return	array
+	 */
+	public function getPermissions()
+	{
+		return array(
+			'can_make_purchasable'		=> 'YesNo',
+			'can_make_unpurchasable'	=> 'YesNo'
+		);
+	}
+	
+	/**
+	 * After change
+	 *
+	 * @param	array	$moderator	The moderator
+	 * @param	array	$changed	Values that were changed
+	 * @return	void
+	 */
+	public function onChange( $moderator, $changed )
+	{
+		
+	}
+	
+	/**
+	 * After delete
+	 *
+	 * @param	array	$moderator	The moderator
+	 * @return	void
+	 */
+	public function onDelete( $moderator )
+	{
+		
+	}
+}
