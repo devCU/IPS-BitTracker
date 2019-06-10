78ef0f3d8763dd6be199c33bc83b7e84bb8e133b
GaalexxC <gary@devcu.com>
2019-06-10 01:46:44 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/EditorLocations/Bitracker.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Editor Extension
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\EditorLocations;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Editor Extension: File descriptions and comments
+ */
+class _Bitracker
+{
+	/**
+	 * Can we use HTML in this editor?
+	 *
+	 * @param	\IPS\Member	$member	The member
+	 * @return	bool|null	NULL will cause the default value (based on the member's permissions) to be used, and is recommended in most cases. A boolean value will override that.
+	 */
+	public function canUseHtml( $member )
+	{
+		return NULL;
+	}
+	
+	/**
+	 * Can we use attachments in this editor?
+	 *
+	 * @param	\IPS\Member					$member	The member
+	 * @param	\IPS\Helpers\Form\Editor	$field	The editor field
+	 * @return	bool|null	NULL will cause the default value (based on the member's permissions) to be used, and is recommended in most cases. A boolean value will override that.
+	 */
+	public function canAttach( $member, $field )
+	{
+		return NULL;
+	}
+
+	/**
+	 * Permission check for attachments
+	 *
+	 * @param	\IPS\Member	$member		The member
+	 * @param	int|null	$id1		Primary ID
+	 * @param	int|null	$id2		Secondary ID
+	 * @param	string|null	$id3		Arbitrary data
+	 * @param	array		$attachment	The attachment data
+	 * @param	bool		$viewOnly	If true, just check if the user can see the attachment rather than download it
+	 * @return	bool
+	 */
+	public function attachmentPermissionCheck( $member, $id1, $id2, $id3, $attachment, $viewOnly=FALSE )
+	{
+		try
+		{
+			$file = \IPS\bitracker\File::load( $id1 );
+			return $file->canView( $member );
+		}
+		catch ( \OutOfRangeException $e )
+		{
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * Attachment lookup
+	 *
+	 * @param	int|null	$id1	Primary ID
+	 * @param	int|null	$id2	Secondary ID
+	 * @param	string|null	$id3	Arbitrary data
+	 * @return	\IPS\Http\Url|\IPS\Content|\IPS\Node\Model
+	 * @throws	\LogicException
+	 */
+	public function attachmentLookup( $id1, $id2, $id3 )
+	{
+		if ( $id2 )
+		{
+			if ( $id3 === 'review' )
+			{
+				return \IPS\bitracker\File\Review::load( $id2 );
+			}
+			else
+			{
+				return \IPS\bitracker\File\Comment::load( $id2 );
+			}
+		}
+		else
+		{
+			return \IPS\bitracker\File::load( $id1 );
+		}
+	}
+}
