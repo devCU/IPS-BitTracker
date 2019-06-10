930979051d8411edf90c246637720a72e904ba02
GaalexxC <gary@devcu.com>
2019-06-09 23:49:54 -0400

    2.0.0 RC 1
    Legacy screenshots for IPS 3.4x

---
+++ bitracker/interface/legacy/screenshot.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker tlegacy Screenshots
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     09 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+\define('REPORT_EXCEPTIONS', TRUE);
+require_once str_replace( 'applications/bitracker/interface/legacy/screenshot.php', '', str_replace( '\\', '/', __FILE__ ) ) . 'init.php';
+\IPS\Session\Front::i();
+
+try
+{
+	/* Get file and data */
+	$file		= \IPS\File::get( 'bitracker_Screenshots', ltrim( \IPS\Request::i()->path, '/' ) );
+
+	$headers	= array_merge( \IPS\Output::getCacheHeaders( time(), 360 ), array( "Content-Disposition" => \IPS\Output::getContentDisposition( 'inline', $file->originalFilename ), "X-Content-Type-Options" => "nosniff" ) );
+
+	/* Send headers and print file */
+	\IPS\Output::i()->sendStatusCodeHeader( 200 );
+	\IPS\Output::i()->sendHeader( "Content-type: " . \IPS\File::getMimeType( $file->originalFilename ) . ";charset=UTF-8" );
+
+	foreach( $headers as $key => $header )
+	{
+		\IPS\Output::i()->sendHeader( $key . ': ' . $header );
+	}
+	\IPS\Output::i()->sendHeader( "Content-Length: " . $file->filesize() );
+
+	$file->printFile();
+	exit;
+}
+catch ( \UnderflowException $e )
+{
+	\IPS\Output::i()->sendOutput( '', 404 );
+}
