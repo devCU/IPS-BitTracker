f966b0a82d658032e5d99fd1d362bc16b05c76b7
GaalexxC <gary@devcu.com>
2019-06-10 01:46:20 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/Announcements/Categories.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker topSubmitters Widget
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\Announcements;
+
+ /* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Announcements Extension: BitTracker Categories
+ */
+class _Categories
+{
+	/**
+	 * @brief	ID Field
+	 */
+	public static $idField = "id";
+	
+	/**
+	 * @brief	Controller classes
+	 */
+	public static $controllers = array( "IPS\\bitracker\\modules\\front\\portal\\main" );
+	
+	/**
+	 * Get Setting Field
+	 *
+	 * @param	\IPS\core\Announcements\Announcement	$announcement
+	 * @return	Form element
+	 */
+	public function getSettingField( $announcement )
+	{
+		return new \IPS\Helpers\Form\Node( 'announce_bitracker_categories', ( $announcement AND $announcement->ids ) ? explode( ",", $announcement->ids ) : 0, FALSE, array( 'class' => 'IPS\bitracker\Category', 'zeroVal' => 'any', 'multiple' => TRUE ), NULL, NULL, NULL, 'announce_bitracker_categories' );
+	}
+}
