054d43f0186b8cac91470742c7d5e70789d963e0
GaalexxC <gary@devcu.com>
2019-06-10 01:47:40 -0400

    2.0.0 RC 1

---
+++ bitracker/extensions/core/MemberRestrictions/Bitracker.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Member Restrictions
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4x
+ * @subpackage	BitTracker
+ * @version     2.0.0 RC 1
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     10 JUN 2019
+ *
+ *                       GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\MemberRestrictions;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * @brief	Member Restrictions: Bitracker
+ */
+class _Bitracker extends \IPS\core\MemberACPProfile\Restriction
+{
+	/**
+	 * Modify Edit Restrictions form
+	 *
+	 * @param	\IPS\Helpers\Form	$form	The form
+	 * @return	void
+	 */
+	public function form( \IPS\Helpers\Form $form )
+	{
+		$form->add( new \IPS\Helpers\Form\YesNo( 'bit_block_submissions', !$this->member->bit_block_submissions ) );
+	}
+	
+	/**
+	 * Save Form
+	 *
+	 * @param	array	$values	Values from form
+	 * @return	array
+	 */
+	public function save( $values )
+	{
+		$return = array();
+		
+		if ( $this->member->bit_block_submissions == $values['bit_block_submissions'] )
+		{
+			$return['bit_block_submissions'] = array( 'old' => $this->member->members_bitoptions['bit_block_submissions'], 'new' => !$values['bit_block_submissions'] );
+			$this->member->bit_block_submissions = !$values['bit_block_submissions'];	
+		}
+		
+		return $return;
+	}
+	
+	/**
+	 * What restrictions are active on the account?
+	 *
+	 * @return	array
+	 */
+	public function activeRestrictions()
+	{
+		$return = array();
+		
+		if ( $this->member->bit_block_submissions )
+		{
+			$return[] = 'restriction_no_bitracker';
+		}
+		
+		return $return;
+	}
+	
+	/**
+	 * Get details of a change to show on history
+	 *
+	 * @param	array	$changes	Changes as set in save()
+	 * @return	array
+	 */
+	public static function changesForHistory( $changes )
+	{
+		if ( isset( $changes['bit_block_submissions'] ) )
+		{
+			return array( \IPS\Member::loggedIn()->language()->addToStack( 'history_restrictions_bitracker_' . intval( $changes['bit_block_submissions']['new'] ) ) );
+		}
+		return array();
+	}
+}
