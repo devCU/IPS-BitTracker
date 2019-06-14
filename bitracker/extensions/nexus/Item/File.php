2a88c8ea5f2502dc473df675003ae8227f88ed45
GaalexxC <gary@devcu.com>
2019-06-14 02:20:53 -0400

    2.0.0 RC 1

--- bitracker/extensions/nexus/Item/File.php
+++ bitracker/extensions/nexus/Item/File.php
@@ -13,7 +13,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     09 JUN 2019
+ * @Updated     2.0.0 RC 1
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -57,7 +57,7 @@ class _File extends \IPS\nexus\Invoice\Item\Purchase
 	/**
 	 * @brief	Icon
 	 */
-	public static $icon = 'cloud-upload';
+	public static $icon = 'cloud';
 	
 	/**
 	 * @brief	Title
