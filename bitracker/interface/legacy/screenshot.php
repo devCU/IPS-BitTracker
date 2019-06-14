4a4b5f5f6221af064ec578d2561801f10b744182
GaalexxC <gary@devcu.com>
2019-06-14 02:24:58 -0400

    Interfaces
    FontAwesome
    clipboard.js

--- bitracker/interface/legacy/screenshot.php
+++ bitracker/interface/legacy/screenshot.php
@@ -3,19 +3,19 @@
  *     Support this Project... Keep it free! Become an Open Source Patron
  *                       https://www.patreon.com/devcu
  *
- * @brief       BitTracker tlegacy Screenshots
+ * @brief       BitTracker Legacy Screens
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.4x
+ * @package     Invision Community Suite 4.2x/4.3x
  * @subpackage	BitTracker
- * @version     2.0.0 RC 1
- * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @version     1.0.3
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     09 JUN 2019
+ * @Updated     05 APR 2019
  *
- *                       GNU General Public License v3.0
+ *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
  *    it under the terms of the GNU General Public License as published by       
  *    the Free Software Foundation, either version 3 of the License, or          
@@ -30,7 +30,7 @@
  *    along with this program.  If not, see http://www.gnu.org/licenses/
  */
 
-\define('REPORT_EXCEPTIONS', TRUE);
+define('REPORT_EXCEPTIONS', TRUE);
 require_once str_replace( 'applications/bitracker/interface/legacy/screenshot.php', '', str_replace( '\\', '/', __FILE__ ) ) . 'init.php';
 \IPS\Session\Front::i();
 
