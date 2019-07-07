24c8adfc1b17c9a41b7a901191ba8a924d480555
GaalexxC <gary@devcu.com>
2019-07-07 17:31:05 -0400

    2.0.0 RC4

--- bitracker/hooks/BitMemberSettingsMenu.php
+++ bitracker/hooks/BitMemberSettingsMenu.php
@@ -18,7 +18,7 @@ public static function hookData() {
     array (
       'selector' => '#elAccountSettingsLink',
       'type' => 'add_after',
-      'content' => '				<li class=\'ipsMenu_item\' id=\'elTrackerSettingsLink\' data-menuItem=\'settings\'><a href=\'{url="app=bitracker&module=system&controller=settings" seoTemplate="settings"}\' title=\'{lang="edit_tracker_settings"}\'>{lang="menu_tracker_settings"}</a></li>',
+      'content' => '				<li class=\'ipsMenu_item\' id=\'elTrackerSettingsLink\' data-menuItem=\'settings\'><a href=\'{url="app=bitracker&module=system&controller=settings" seoTemplate="bitracker_settings"}\' title=\'{lang="edit_tracker_settings"}\'>{lang="menu_tracker_settings"}</a></li>',
     ),
   ),
 ), parent::hookData() );
