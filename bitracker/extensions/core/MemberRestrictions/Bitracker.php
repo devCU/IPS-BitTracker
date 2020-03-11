60b7de95c8ef55d5969e73f330cbf570813d975a
GaalexxC <gary@devcu.com>
2020-03-11 02:03:46 -0400

    RC 1
    PHP root class \

--- bitracker/extensions/core/MemberRestrictions/Bitracker.php
+++ bitracker/extensions/core/MemberRestrictions/Bitracker.php
@@ -9,11 +9,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4x
  * @subpackage	BitTracker
- * @version     2.0.1 Beta Build
+ * @version     2.1.0 RC 1
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     28 JUL 2019
+ * @Updated     11 MAR 2020
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -101,7 +101,7 @@ class _Bitracker extends \IPS\core\MemberACPProfile\Restriction
 	{
 		if ( isset( $changes['bit_block_submissions'] ) )
 		{
-			return array( \IPS\Member::loggedIn()->language()->addToStack( 'history_restrictions_bitracker_' . intval( $changes['bit_block_submissions']['new'] ) ) );
+			return array( \IPS\Member::loggedIn()->language()->addToStack( 'history_restrictions_bitracker_' . \intval( $changes['bit_block_submissions']['new'] ) ) );
 		}
 		return array();
 	}
