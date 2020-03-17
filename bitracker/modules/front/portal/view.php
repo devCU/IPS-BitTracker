545ebe7f6f6a83ea2ab529609d1d9a74f393d3f9
GaalexxC <gary@devcu.com>
2020-03-17 02:14:49 -0400

    RC1

--- bitracker/modules/front/portal/view.php
+++ bitracker/modules/front/portal/view.php
@@ -9,11 +9,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4x
  * @subpackage	BitTracker
- * @version     2.0.1 Beta Build
+ * @version     2.1.0 RC 1
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     28 JUL 2019
+ * @Updated     17 MAR 2020
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -300,6 +300,8 @@ class _view extends \IPS\Content\Controller
 		
 		$this->file->purchasable = (bool) \IPS\Request::i()->value;
 		$this->file->save();
+		
+		\IPS\Session::i()->modLog( 'modlog__action_purchasestatus', array( (string) $this->file->url() => FALSE, $this->file->name => FALSE ), $this->file );
 		\IPS\Output::i()->redirect( $this->file->url(), 'saved' );
 	}
 	
