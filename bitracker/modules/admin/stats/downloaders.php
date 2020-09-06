608f8d8a1dabfa14ef7dd328ebce7dae85da3b1f
GaalexxC <gary@devcu.com>
2020-09-06 01:30:26 -0400

    2.2.0 Final

--- bitracker/modules/admin/stats/downloaders.php
+++ bitracker/modules/admin/stats/downloaders.php
@@ -1,16 +1,19 @@
 <?php
 /**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                      https://www.devcu.com/donate/
+ *
  * @brief       BitTracker Application Class
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.2x
+ * @package     Invision Community Suite 4.4.10
  * @subpackage	BitTracker
- * @version     1.0.0 Beta 1
- * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
- * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @version     2.2.0 Final
+ * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     19 FEB 2018
+ * @Updated     06 SEP 2020
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -30,7 +33,7 @@
 namespace IPS\bitracker\modules\admin\stats;
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
-if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 {
 	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
 	exit;
@@ -41,6 +44,9 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
  */
 class _downloaders extends \IPS\Dispatcher\Controller
 {
+	/**
+	 * @brief	Number of results per page
+	 */
 	const PER_PAGE = 25;
 	
 	/**
@@ -61,40 +67,50 @@ class _downloaders extends \IPS\Dispatcher\Controller
 	 */
 	protected function manage()
 	{
-		$values = NULL;
 		$where = array( array( 'dmid>0' ) );
 		
 		if ( isset( \IPS\Request::i()->form ) )
 		{
 			$form = new \IPS\Helpers\Form( 'form', 'go' );
-			$form->add( new \IPS\Helpers\Form\DateRange( 'stats_date_range' ), NULL, FALSE, array( 'start' => array( 'max' => new \IPS\DateTime() ), 'end' => array( 'max' => new \IPS\DateTime() ) ));
-			
-			if ( $values = $form->values() )
-			{
-				if ( $values['stats_date_range']['start'] )
-				{
-					$where[] = array( 'dtime>?', $values['stats_date_range']['start']->getTimestamp() );
-				}
-				if ( $values['stats_date_range']['end'] )
-				{
-					$where[] = array( 'dtime<?', $values['stats_date_range']['end']->getTimestamp() );
-				}
-			}
-			else
+
+			$default = array(
+				'start' => \IPS\Request::i()->start ? \IPS\DateTime::ts( \IPS\Request::i()->start ) : NULL,
+				'end' => \IPS\Request::i()->end ? \IPS\DateTime::ts( \IPS\Request::i()->end ) : NULL
+			);
+
+			$form->add( new \IPS\Helpers\Form\DateRange( 'stats_date_range', $default, FALSE, array( 'start' => array( 'max' => \IPS\DateTime::ts( time() )->setTime( 0, 0, 0 ), 'time' => FALSE ), 'end' => array( 'max' => \IPS\DateTime::ts( time() )->setTime( 23, 59, 59 ), 'time' => FALSE ) ) ) );
+
+			if ( !$values = $form->values() )
 			{
 				\IPS\Output::i()->output = $form;
 				return;
 			}
 		}
+
+		/* Figure out start and end parameters for links */
+		$params = array(
+			'start' => !empty( $values['stats_date_range']['start'] ) ? $values['stats_date_range']['start']->getTimestamp() : \IPS\Request::i()->start,
+			'end' => !empty( $values['stats_date_range']['end'] ) ? $values['stats_date_range']['end']->getTimestamp() : \IPS\Request::i()->end
+		);
+
+		if( $params['start'] )
+		{
+			$where[] = array( 'dtime>?', $params['start'] );
+		}
+
+		if( $params['end'] )
+		{
+			$where[] = array( 'dtime<?', $params['end'] );
+		}
 		
-		$page = isset( \IPS\Request::i()->page ) ? intval( \IPS\Request::i()->page ) : 1;
+		$page = isset( \IPS\Request::i()->page ) ? \intval( \IPS\Request::i()->page ) : 1;
 
 		if( $page < 1 )
 		{
 			$page = 1;
 		}
 
-		$select = \IPS\Db::i()->select( 'dmid, COUNT(*) as bitracker', 'bitracker_torrents', $where, 'bitracker DESC', array( ( $page - 1 ) * static::PER_PAGE, static::PER_PAGE ), 'dmid', NULL, \IPS\Db::SELECT_SQL_CALC_FOUND_ROWS );
+		$select = \IPS\Db::i()->select( 'dmid, count(*) as bitracker', 'bitracker_torrents', $where, 'bitracker DESC', array( ( $page - 1 ) * static::PER_PAGE, static::PER_PAGE ), 'dmid', NULL, \IPS\Db::SELECT_SQL_CALC_FOUND_ROWS );
 		$mids = array();
 		
 		foreach( $select as $row )
@@ -104,13 +120,13 @@ class _downloaders extends \IPS\Dispatcher\Controller
 		
 		$members = array();
 		
-		if ( count( $mids ) )
+		if ( \count( $mids ) )
 		{
 			$members = iterator_to_array( \IPS\Db::i()->select( '*', 'core_members', array( \IPS\Db::i()->in( 'member_id', $mids ) ) )->setKeyField('member_id') );
 		}
 		
 		$pagination = \IPS\Theme::i()->getTemplate( 'global', 'core', 'global' )->pagination(
-			\IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=downloaders' )->setQueryString( $values ),
+			\IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=downloaders' )->setQueryString( $params ),
 			ceil( $select->count( TRUE ) / static::PER_PAGE ),
 			$page,
 			static::PER_PAGE,
@@ -121,7 +137,7 @@ class _downloaders extends \IPS\Dispatcher\Controller
 			'settings'	=> array(
 				'title'		=> 'stats_date_range',
 				'icon'		=> 'calendar',
-				'link'		=> \IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=downloaders&form=1' )->setQueryString( $values ),
+				'link'		=> \IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=downloaders&form=1' )->setQueryString( $params ),
 				'data'		=> array( 'ipsDialog' => '', 'ipsDialog-title' => \IPS\Member::loggedIn()->language()->addToStack('stats_date_range') )
 			)
 		);
