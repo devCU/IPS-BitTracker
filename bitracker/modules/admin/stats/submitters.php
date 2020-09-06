ddf366c8433ec3e1cd721607c0ccb8464001e6d0
GaalexxC <gary@devcu.com>
2020-09-06 02:42:38 -0400

    2.2.0 Final

--- bitracker/modules/admin/stats/submitters.php
+++ bitracker/modules/admin/stats/submitters.php
@@ -1,19 +1,19 @@
 <?php
 /**
  *     Support this Project... Keep it free! Become an Open Source Patron
- *                       https://www.patreon.com/devcu
- *
+ *                      https://www.devcu.com/donate/
+ * 
  * @brief       BitTracker Top Submitters
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.4x
+ * @package     Invision Community Suite 4.4.10
  * @subpackage	BitTracker
- * @version     2.0.1 Beta Build
+ * @version     2.2.0 Final
  * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     28 JUL 2019
+ * @Updated     06 SEP 2020
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -44,6 +44,9 @@ if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
  */
 class _submitters extends \IPS\Dispatcher\Controller
 {
+	/**
+	 * @brief	Number of results per page
+	 */
 	const PER_PAGE = 25;
 	
 	/**
@@ -64,40 +67,50 @@ class _submitters extends \IPS\Dispatcher\Controller
 	 */
 	protected function manage()
 	{
-		$values = NULL;
 		$where = array();
-		
+
 		if ( isset( \IPS\Request::i()->form ) )
 		{
 			$form = new \IPS\Helpers\Form( 'form', 'go' );
-			$form->add( new \IPS\Helpers\Form\DateRange( 'stats_date_range' ), NULL, FALSE, array( 'start' => array( 'max' => new \IPS\DateTime() ), 'end' => array( 'max' => new \IPS\DateTime() ) ) );
-			
-			if ( $values = $form->values() )
-			{
-				if ( $values['stats_date_range']['start'] )
-				{
-					$where[] = array( 'file_submitted>?', $values['stats_date_range']['start']->getTimestamp() );
-				}
-				if ( $values['stats_date_range']['end'] )
-				{
-					$where[] = array( 'file_submitted<?', $values['stats_date_range']['end']->getTimestamp() );
-				}
-			}
-			else
+
+			$default = array(
+				'start' => \IPS\Request::i()->start ? \IPS\DateTime::ts( \IPS\Request::i()->start ) : NULL,
+				'end' => \IPS\Request::i()->end ? \IPS\DateTime::ts( \IPS\Request::i()->end ) : NULL
+			);
+
+			$form->add( new \IPS\Helpers\Form\DateRange( 'stats_date_range', $default, FALSE, array( 'start' => array( 'max' => \IPS\DateTime::ts( time() )->setTime( 0, 0, 0 ), 'time' => FALSE ), 'end' => array( 'max' => \IPS\DateTime::ts( time() )->setTime( 23, 59, 59 ), 'time' => FALSE ) ) ) );
+
+			if ( !$values = $form->values() )
 			{
 				\IPS\Output::i()->output = $form;
 				return;
 			}
 		}
+
+		/* Figure out start and end parameters for links */
+		$params = array(
+			'start' => !empty( $values['stats_date_range']['start'] ) ? $values['stats_date_range']['start']->getTimestamp() : \IPS\Request::i()->start,
+			'end' => !empty( $values['stats_date_range']['end'] ) ? $values['stats_date_range']['end']->getTimestamp() : \IPS\Request::i()->end
+		);
+
+		if( $params['start'] )
+		{
+			$where[] = array( 'file_submitted>?', $params['start'] );
+		}
+
+		if( $params['end'] )
+		{
+			$where[] = array( 'file_submitted<?', $params['end'] );
+		}
 		
-		$page = isset( \IPS\Request::i()->page ) ? intval( \IPS\Request::i()->page ) : 1;
+		$page = isset( \IPS\Request::i()->page ) ? \intval( \IPS\Request::i()->page ) : 1;
 
 		if( $page < 1 )
 		{
 			$page = 1;
 		}
 
-		$select = \IPS\Db::i()->select( 'file_submitter, COUNT(*) as files', 'bitracker_torrents', $where, 'files DESC', array( ( $page - 1 ) * static::PER_PAGE, static::PER_PAGE ), 'file_submitter', NULL, \IPS\Db::SELECT_SQL_CALC_FOUND_ROWS )->join( 'core_members', 'core_members.member_id=bitracker_torrents.file_submitter' );
+		$select = \IPS\Db::i()->select( 'file_submitter, count(*) as files', 'bitracker_torrents', $where, 'files DESC', array( ( $page - 1 ) * static::PER_PAGE, static::PER_PAGE ), 'file_submitter', NULL, \IPS\Db::SELECT_SQL_CALC_FOUND_ROWS )->join( 'core_members', 'core_members.member_id=bitracker_torrents.file_submitter' );
 		$mids = array();
 		
 		foreach( $select as $row )
@@ -113,7 +126,7 @@ class _submitters extends \IPS\Dispatcher\Controller
 		}
 		
 		$pagination = \IPS\Theme::i()->getTemplate( 'global', 'core', 'global' )->pagination(
-			\IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=submitters' )->setQueryString( $values ),
+			\IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=submitters' )->setQueryString( $params ),
 			ceil( $select->count( TRUE ) / static::PER_PAGE ),
 			$page,
 			static::PER_PAGE,
@@ -124,7 +137,7 @@ class _submitters extends \IPS\Dispatcher\Controller
 			'settings'	=> array(
 				'title'		=> 'stats_date_range',
 				'icon'		=> 'calendar',
-				'link'		=> \IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=submitters&form=1' )->setQueryString( $values ),
+				'link'		=> \IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=submitters&form=1' )->setQueryString( $params ),
 				'data'		=> array( 'ipsDialog' => '', 'ipsDialog-title' => \IPS\Member::loggedIn()->language()->addToStack('stats_date_range') )
 			)
 		);
