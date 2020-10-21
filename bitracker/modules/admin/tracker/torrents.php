82773d887388a95d418c944fc7bdd53273e0c44f
GaalexxC <gary@devcu.com>
2020-10-21 12:26:43 -0400

    IPS 4.5x

--- bitracker/modules/admin/tracker/torrents.php
+++ bitracker/modules/admin/tracker/torrents.php
@@ -4,13 +4,13 @@
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.2x
+ * @package     Invision Community Suite 4.5x
  * @subpackage	BitTracker
- * @version     1.0.0 Beta 1
- * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
- * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @version     2.5.0 Stable
+ * @source      https://github.com/devCU/IPS-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     27 MAR 2018
+ * @Updated     21 OCT 2020
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -42,6 +42,11 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
  */
 class _torrents extends \IPS\Dispatcher\Controller
 {
+	/**
+	 * @brief	Has been CSRF-protected
+	 */
+	public static $csrfProtected = TRUE;
+	
 	/**
 	 * Execute
 	 *
