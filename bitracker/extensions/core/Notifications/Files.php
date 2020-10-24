fb4eda10523c3a1c7de3ffe988ab2e750115afc6
GaalexxC <gary@devcu.com>
2020-10-24 09:12:30 -0400

    IPS 4.5x

--- bitracker/extensions/core/Notifications/Files.php
+++ bitracker/extensions/core/Notifications/Files.php
@@ -7,13 +7,13 @@
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.4.10
+ * @package     Invision Community Suite 4.5x
  * @subpackage	BitTracker
- * @version     2.2.0 Final
- * @source      https://github.com/GaalexxC/IPS-4.4-BitTracker
+ * @version     2.5.0 Stable
+ * @source      https://github.com/devCU/IPS-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     31 AUG 2020
+ * @Updated     24 OCT 2020
  *
  *                       GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -45,22 +45,31 @@ if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 class _Files
 {
 	/**
-	 * Get configuration
+	 * Get fields for configuration
 	 *
-	 * @param	\IPS\Member	$member	The member
+	 * @param	\IPS\Member|null	$member		The member (to take out any notification types a given member will never see) or NULL if this is for the ACP
 	 * @return	array
 	 */
-	public function getConfiguration( $member )
-	{		
+	public static function configurationOptions( \IPS\Member $member = NULL )
+	{
 		return array(
-			'new_torrent_version'	=> array( 'default' => array( 'email' ), 'disabled' => array() ),
+			'new_torrent_version'	=> array(
+				'type'				=> 'standard',
+				'notificationTypes'	=> array( 'new_torrent_version' ),
+				'title'				=> 'notifications__bitracker_Files',
+				'showTitle'			=> FALSE,
+				'description'		=> 'notifications__bitracker_Files_desc',
+				'default'			=> array( 'inline', 'push', 'email' ),
+				'disabled'			=> array()
+			)
 		);
 	}
 	
 	/**
-	 * Parse notification: new_content
+	 * Parse notification: new_file_version
 	 *
 	 * @param	\IPS\Notification\Inline	$notification	The notification
+	 * @param	bool						$htmlEscape		TRUE to escape HTML in title
 	 * @return	array
 	 * @code
 	 	return array(
@@ -73,20 +82,42 @@ class _Files
 	 	);
 	 * @endcode
 	 */
-	public function parse_new_torrent_version( $notification )
+	public function parse_new_file_version( $notification, $htmlEscape=TRUE )
 	{
 		$item = $notification->item;
 		if ( !$item )
 		{
 			throw new \OutOfRangeException;
 		}
-
+                
 		return array(
-			'title'		=> \IPS\Member::loggedIn()->language()->addToStack( 'notification__new_torrent_version', FALSE, array( 'sprintf' => array( $item->author()->name, $item->mapped('title') ) ) ),
+			'title'		=> ( $item->container()->version_numbers ) ? \IPS\Member::loggedIn()->language()->addToStack( 'notification__new_torrent_version_with', FALSE, array( 'sprintf' => array( $item->author()->name, $item->version, $item->mapped('title') ) ) ) : \IPS\Member::loggedIn()->language()->addToStack( 'notification__new_torrent_version', FALSE, array( 'sprintf' => array( $item->author()->name, $item->mapped('title') ) ) ),
 			'url'		=> $notification->item->url(),
 			'content'	=> $notification->item->content(),
 			'author'	=> $notification->extra ?: $notification->item->author(),
 			'unread'	=> (bool) ( $item->unread() )
+		);
+	}
+	
+	/**
+	 * Parse notification for mobile: new_torrent_version
+	 *
+	 * @param	\IPS\Lang			$language	The language that the notification should be in
+	 * @param	\IPS\bitracker\File	$file		The file
+	 * @return	array
+	 */
+	public static function parse_mobile_new_torrent_version( \IPS\Lang $language, \IPS\bitracker\File $file )
+	{
+		return array(
+			'body'		=> $language->addToStack( 'notification__new_torrent_version', FALSE, array( 'htmlsprintf' => array(
+				$file->author()->name,
+				$file->mapped('title')
+			) ) ),
+			'data'		=> array(
+				'url'		=> (string) $file->url(),
+				'author'	=> $file->author()
+			),
+			'channelId'	=> 'files',
 		);
 	}
 }
